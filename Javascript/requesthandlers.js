038b598cd49fd3b74f525d7789d40e9a34248db3
Chuck Dries <chuck@chuckdries.com>
2016-06-14 13:07:05 -0700

    RequestHandlers can respond information

--- Javascript/requesthandlers.js
+++ Javascript/requesthandlers.js
@@ -1,11 +1,23 @@
-function start() {
+var exec = require("child_process").exec;
+function start(response) {
     console.log("Request for handler 'start' was called");
+    exec("ls -lah", function (error, stdout, stderr){
+        response.writeHead(200, {"Content-Type":"text/plain"});
+        response.write(stdout);
+        response.end();
+    });
 }
-function upload() {
+function upload(response) {
     console.log("Request for handler 'upload' was called");
+    response.writeHead(200, {"Content-Type":"text/plain"});
+    response.write("GET YOUR ENGINES STARTED!!!!!");
+    response.end();
 }
-function camp() {
+function camp(response) {
     console.log("Request for handler 'camp' was called");
+    response.writeHead(200, {"Content-Type":"text/plain"});
+    response.write("I'm going camping and I'm going to bring...");
+    response.end();
 }
 
 exports.start = start;
--- Javascript/server.js
+++ Javascript/server.js
@@ -4,18 +4,13 @@ var url = require("url");
 function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + "received");
 
-        route(handle, pathname);
-
-        response.writeHead(200, {
-            "Content-Type": "text/plain"
-        });
-        response.write("Hello world! \n");
-        response.end();
+        route(handle, pathname, response);
     }
 
-    var server = http.createServer(onRequest);
-    server.listen(8888);
+    http.createServer(onRequest).listen(8888);
+    
     console.log("Server listening on port 8888");
 }
 exports.start = start;
