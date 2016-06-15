16776135c6ab50b23184c7eacb1c9942c149497c
Chuck Dries <chuck@chuckdries.com>
2016-06-14 19:53:03 -0700

    Continued following tutorial on basic Node usage

--- Javascript/requesthandlers.js
+++ Javascript/requesthandlers.js
@@ -1,24 +1,37 @@
-var exec = require("child_process").exec;
-function start(response) {
+// var exec = require("child_process").exec;
+var querystring = require("querystring");
+var fs = require("fs")
+
+function start(response, postData) {
     console.log("Request for handler 'start' was called");
-    exec("ls -lah", function (error, stdout, stderr){
-        response.writeHead(200, {"Content-Type":"text/plain"});
-        response.write(stdout);
-        response.end();
-    });
+    var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html"/>'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.write(body);
+    response.end();
 }
-function upload(response) {
+function upload(response, postData) {
     console.log("Request for handler 'upload' was called");
     response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("GET YOUR ENGINES STARTED!!!!!");
+    response.write("I'm going fishing and I'm going to bring... " + querystring.parse(postData).text);
     response.end();
 }
-function camp(response) {
-    console.log("Request for handler 'camp' was called");
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("I'm going camping and I'm going to bring...");
-    response.end();
+function show(response){
+    console.log("Request handler 'show' was called.");
+    response.writeHead(200, {"Content-Type":"image/png"});
+    fs.createReadStream("/tmp/test.png").pipe(response);
+
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
