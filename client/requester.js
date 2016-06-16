94319e3535c9b98b0ed86016f2cc19b344155352
Chuck Dries <chuck@chuckdries.com>
2016-06-15 23:17:13 -0700

    Last push of the night: add todo to comments for tomorrow

--- client/requester.js
+++ client/requester.js
@@ -3,8 +3,10 @@
 //Apparently  in javascript if you save an anonymous function with specific arguments to a variable or an array,
 //it preserves the state of those arguments. I guess that's how closures work lol.
 //Anyway now we save these functions to an array, but unlike instantiating these functions as objects,
-//they aren't evaluated when we create them. We can populate an array with requesters and choose
+//they aren't evaluated when we create/save them. We can populate an array with requesters and choose
 //to wait to dispatch them if we want. Kinda cool :)
+
+//TODO: investigate http://stackoverflow.com/questions/8164989/javascript-self-executing-anonymous-functions-and-callback for alternative syntax
 var reqs = [];
 
 function requester(url, callbackFunc, parse, params, idx) {
