f8443fe1e1be0dc6f85ca26ea547f00e3fe1846f
Chuck Dries <chuck@chuckdries.com>
2016-07-02 14:35:38 -0700

    Begin implementing template engine on client side

---
+++ client/templated/script/browserrequester.js
@@ -0,0 +1,38 @@
+//TODO: we should probably pass request objects around instead of ID's, it seems like it'd make more sense
+var reqs = [];
+
+//private, don't use
+function requester(url, callbackFunc, parse, params, idx) {
+    return function () {
+        var xcallback = function () {
+            if (parse === false) {
+                callbackFunc(this.responseText, params, idx); //but params works just fine?
+            } else {
+                callbackFunc(JSON.parse(this.responseText), params, idx);
+            }
+        }
+        var xhr = new XMLHttpRequest;
+        xhr.addEventListener("load", xcallback);
+        xhr.open("GET", url);
+        xhr.send();
+    }
+
+}
+function call(fn){
+    fn();
+}
+//use this
+function addRequest(url, callBackFunc, parse, params){
+    var idx = reqs.length;
+    reqs.push(requester(url, callBackFunc, parse, params, idx));
+    dispatch(idx);
+    return idx;
+}
+
+function dispatch(idx){
+    call(reqs[idx]);
+}
+
+function dispatchAll(){
+    reqs.forEach(call);
+}
