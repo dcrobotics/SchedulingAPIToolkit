74967b0d9bef04f1259088ee85c303d546fee04f
Kent Kuhler <KKuhler@gmail.com>
2016-07-10 14:21:36 -0700

    Added express framework
    ToDo Passport is next

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
-var http = require('http');
+//var http = require('http');
+var express = require('express');
 var url  = require('url');
 var WP   = require('wpapi');
 
@@ -21,36 +22,39 @@ exports.WP_JSON_HEAD  = WP_JSON_HEAD;
 exports.wpEp          = wpEp;
 exports.wpEpDiscovery = wpEpDiscovery;
 
-var handle = {};
-// Assign handlers for the root of the pathname
-handle['']        = wpRequestHandlers.root;
-handle['refresh'] = wpRequestHandlers.refresh;
-handle['wp']      = wpRequestHandlers.wpParse;
-handle['ee']      = eeRequestHandlers.eeParse;
+const WS_PORT = 8080;
+var webServer = express();
+webServer.listen(WS_PORT, function() {console.log('DCR API server listening on port ' + WS_PORT);});
 
-function route(request, splitPath, query, response) {
-  //console.log('Routing a request for /' + splitPath[1]);
-  // Use the root of the pathname to determine a handler
-  if (typeof handle[splitPath[1]] === 'function'){
-    handle[splitPath[1]](request, splitPath, query, response);
-  } else {
-    console.log('No request handler found for ' + splitPath[1] + ' of ' + request.url);
-    response.writeHead(404, {'Content-Type':'text/plain'});
-    response.write('404 Not Found');
-    response.end();
-  }
-}
+// Root route
+webServer.get('/',function(req, rsp) {
+  console.log('Request for ' + req.url + ' received from ' + req.headers['x-forwarded-for']);
+  util.sendResponse(rsp, util.contType.TEXT, 'Welcome to DCR\'s node server' );
+});
+// /reresh route
+webServer.get('/refresh',function(req, rsp) {
+  wpEpDiscovery = WP.discover( DATA_SITE );
+  util.sendResponse(rsp, util.contType.TEXT, 'Discovery data has been refreshed.' );
+});
 
-function startServer() {
-  function onRequest(request, response) {
-    var path = url.parse(request.url).pathname;
-    var splitPath = path.split('/')
-    var query = url.parse(request.url).query;
-    console.log('Request for ' + request.url + ' received from ' + request.headers['x-forwarded-for']);
-    route(request, splitPath, query, response);
-  }
-  http.createServer(onRequest).listen(8080);
-  console.log('Server listening on port 8080');
-}
+// Wordpress default REST API
+webServer.get('/wp(/*)?',function(req, rsp) {
+  console.log('Request for ' + req.url + ' received from ' + req.headers['x-forwarded-for']);
+  var path = url.parse(req.url).pathname;
+  var splitPath = path.split('/')
+  var query = url.parse(req.url).query;
+  console.log('Request for ' + req.url + ' received from ' + req.headers['x-forwarded-for']);
+  wpRequestHandlers.wpParse(req, splitPath, query, rsp);
+});
 
-startServer();
+// Event Espresso REST API
+webServer.get('/ee(/*)?',function(req, rsp) {
+  console.log('Request for ' + req.url + ' received from ' + req.headers['x-forwarded-for']);
+  var path = url.parse(req.url).pathname;
+  var splitPath = path.split('/')
+  var query = url.parse(req.url).query;
+  console.log('Request for ' + req.url + ' received from ' + req.headers['x-forwarded-for']);
+  eeRequestHandlers.eeParse(req, splitPath, query, rsp);
+});
+
+
